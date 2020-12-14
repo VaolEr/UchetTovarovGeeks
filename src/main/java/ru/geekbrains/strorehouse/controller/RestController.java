541e4319101b93fb8f5ff5cf8337bdca53522249
Egor Ogdanets <ogdanets2010@yandex.ru>
2020-12-14 22:06:38 +0300

    Add getProductByID, addProduct and createProduct methods

--- src/main/java/ru/geekbrains/strorehouse/controller/RestController.java
+++ src/main/java/ru/geekbrains/strorehouse/controller/RestController.java
@@ -2,16 +2,34 @@ package ru.geekbrains.strorehouse.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import ru.geekbrains.strorehouse.dao.ProductDAO;
 import ru.geekbrains.strorehouse.model.Product;
+import ru.geekbrains.strorehouse.model.ProductWithoutID;
+import ru.geekbrains.strorehouse.model.StatusCode;
+import ru.geekbrains.strorehouse.model.StatusCodeType;
 
 import java.util.List;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
+
+    //GET на получение списка товара.
+    //С параметрами в URL по ограничению выдачи (по складу, по бренду, диапазон).
+    //Возвращает json массив с товарами.
+
+    //GET на получение одного товара.
+    //В ответ ждем json с его характиристиками
+
+    //POST на создание одного товара.
+    //В теле запроса в json передаем характеристики. В ответ получаем код (успех/фейл)
+
+    //POST / PUT на обновление одного товара.
+    //В Урле будет id товара, в теле запроса в json его новые параметры.
+    //В ответ будем ждать код (успех / фейл)
+
+    //DELETE на удаление товара. Удаление по id из урла.
+    //В ответ будем ждать код (успех / фейл)
 
     @Autowired
     private ProductDAO productDAO;
@@ -27,11 +45,34 @@ public class RestController {
     //(напр, список продуктов по каждому поставщику)
     @RequestMapping(value = "/products",
             method = RequestMethod.GET,
-            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+            produces = {MediaType.APPLICATION_JSON_VALUE})
     @ResponseBody
     public List<Product> getProducts(){
         List<Product> list = productDAO.getAllProducts();
         return list;
     }
+
+    @RequestMapping(value = "/product/{id}",
+            method = RequestMethod.GET,
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseBody
+    public Product addProduct(@PathVariable("id") String id) {
+        return productDAO.getProduct(id);
+    }
+
+    @RequestMapping(value = "/product",
+            method = RequestMethod.POST,
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseBody
+    public StatusCode addProduct(@RequestBody ProductWithoutID product){
+        if (product == null) {
+            return new StatusCode(StatusCodeType.REQUEST_ERROR);
+        } else {
+            return new StatusCode(productDAO.addProduct(product));
+        }
+    }
+
+
+
 
 }
--- src/main/java/ru/geekbrains/strorehouse/dao/ProductDAO.java
+++ src/main/java/ru/geekbrains/strorehouse/dao/ProductDAO.java
@@ -2,13 +2,33 @@ package ru.geekbrains.strorehouse.dao;
 
 import org.springframework.stereotype.Repository;
 import ru.geekbrains.strorehouse.model.Product;
+import ru.geekbrains.strorehouse.model.ProductWithoutID;
+import ru.geekbrains.strorehouse.model.StatusCodeType;
 
 import java.util.*;
 
 @Repository
 public class ProductDAO {
 
-    private static final Map<String, Product> productMap = new HashMap<String, Product>();
+    //GET на получение списка товара.
+    //С параметрами в URL по ограничению выдачи (по складу, по бренду, диапазон).
+    //Возвращает json массив с товарами.
+
+    //GET на получение одного товара.
+    //В ответ ждем json с его характиристиками
+
+    //POST на создание одного товара.
+    //В теле запроса в json передаем характеристики. В ответ получаем код (успех/фейл)
+
+    //POST / PUT на обновление одного товара.
+    //В Урле будет id товара, в теле запроса в json его новые параметры.
+    //В ответ будем ждать код (успех / фейл)
+
+    //DELETE на удаление товара. Удаление по id из урла.
+    //В ответ будем ждать код (успех / фейл)
+
+    private static final Map<Long, Product> productMap = new HashMap<Long, Product>();
+    private long productCount = 3;
 
     static {
         initProducts();
@@ -16,9 +36,14 @@ public class ProductDAO {
 
     private static void initProducts() {
         //Здесь, как я предполагаю, будем подключаться к БД по JDBC
-        Product product = new Product(1,"New Article","New Name",1,1, 1);
+        Product product1 = new Product(1,"New Article 1","New Name 1",1,1, 1);
+        Product product2 = new Product(2,"New Article 2","New Name 2",2,2, 2);
+        Product product3 = new Product(3,"New Article 3","New Name 3",3,3, 3);
 
-        productMap.put(String.valueOf(product.getId()),product);
+
+        productMap.put(product1.getId(),product1);
+        productMap.put(product2.getId(),product2);
+        productMap.put(product3.getId(),product3);
     }
 
     public List<Product> getAllProducts(){
@@ -26,5 +51,26 @@ public class ProductDAO {
         List<Product> list = new ArrayList<Product>();
         list.addAll(c);
         return list;
+    }
+
+    public Product getProduct(String id) {
+        return productMap.get(id);
+    }
+
+    public StatusCodeType addProduct(ProductWithoutID productWithoutID) {
+        try{
+            Product product = createProduct(productWithoutID);
+            productMap.put(product.getId(), product);
+        } catch (Exception ex){
+            ex.printStackTrace();
+            return StatusCodeType.DB_ERROR;
+        }
+        return StatusCodeType.SUCCESS;
+    }
+
+    public Product createProduct(ProductWithoutID product){
+        productCount++;
+        return new Product(productCount,product.getArticle() + productCount,product.getName() + productCount,
+                product.getTypeID(), product.getSupplierID(), product.getWarehouseID());
     }
 }
