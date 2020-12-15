ed0258f3ed63e84e52d920f753d3d726f59ea44e
zedko <zedko@bk.ru>
2020-12-15 10:46:10 +0300

    added CORS headers module

--- project/project/settings.py
+++ project/project/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'stockapp',
     'rest_framework'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +130,5 @@ REST_FRAMEWORK = {
 }
 
 BACKEND_API_BASE_URL = 'http://localhost:8090/'
+
+CORS_ALLOW_ALL_ORIGINS = True
